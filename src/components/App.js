97d93154231f66f02e25c97729ce2e4f6bc73c51
arbp97 <arbp97@outlook.com>
2022-05-09 18:15:56 -0300

    refactor: rewrite service worker with mixed strategy

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-function App() {
+import "../theme/App.css";
+
+const App = () => {
   return (
     <div className="App">
       <p>Test React App</p>
     </div>
   );
-}
+};
 
 export default App;
