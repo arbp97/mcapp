874764b1e326adfcbfc5dfcc0ab4a80426c27b33
arbp97 <arbp97@outlook.com>
2022-05-11 17:46:20 -0300

    feat: add comments on nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import "../theme/Navigation.css";
 import { createRef, useState } from "react";
 
 const Navigation = () => {
+  // nav button values
   const buttons = {
     home: { text: "Home", img: "logo-black.png" },
     orders: { text: "Pedidos", img: "fries.png" },
@@ -10,19 +11,22 @@ const Navigation = () => {
     more: { text: "Más", img: "more.png" },
   };
 
+  // default view is home view
   const [active, setActive] = useState("home");
 
   return (
     <nav className="nav-container">
       <ul className="nav-list">
         {Object.entries(buttons).map(([key, value]) => {
+          // load all buttons listed before
           return (
             <li
               className={
+                // if key is the actual active key, set active class
                 active == key ? "nav-list-item active" : "nav-list-item"
               }
               key={key}
-              onClick={() => setActive(key)}
+              onClick={() => setActive(key)} // change active button on click
             >
               <img src={"img/nav/" + value.img}></img>
               <p>{value.text}</p>
