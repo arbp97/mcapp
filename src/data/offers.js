67e9dd3057254fe9a7d29dc0781aad6b28357d70
Santiago Martinez <smartinezvila29@gmail.com>
2022-05-25 00:15:14 -0300

    Avance seccion ofertas

---
+++ src/data/offers.js
@@ -0,0 +1,35 @@
+// categories & products
+const offers = {
+      category: "Ofertas",
+      items: [
+        {
+          img: "big_mac.png", // first img used as category image
+          title: "Big Mac",
+          description: "La hamburguesa más famosa del mundo. Un sabor único.",
+          price: "$700",
+        },
+        {
+          img: "mcnifica.png",
+          title: "McNífica",
+          description:
+            "En un mundo donde todos buscan lo nuevo, la McNífica viene a rectificar lo bueno de ser clásico.",
+            price: "$700",
+        },
+        {
+          img: "mc_pollo.png",
+          title: "McPollo",
+          description:
+            "El auténtico sabor del pollo lo encontrás en nuestro clásico McPollo.",
+            price: "$700",
+        },
+        {
+          img: "mc_pollo_2.png",
+          title: "McPollo doble",
+          description: "El clásico McPollo, esta vez con doble sabor.",
+          price: "$700",
+        },
+      ],
+  };
+  
+  export default offers;
+  
