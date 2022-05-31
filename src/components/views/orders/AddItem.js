e070426d22cdbb7ff55e3732d616c86b715f7fae
arbp97 <alanblangille@gmail.com>
2022-05-30 22:08:33 -0300

    feat: add counter to add item button
    Signed-off-by: arbp97 <alanblangille@gmail.com>

--- src/components/views/orders/AddItem.js
+++ src/components/views/orders/AddItem.js
@@ -1,9 +1,11 @@
 import "./AddItem.css";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const AddItem = () => {
   const location = useLocation();
   const data = location.state;
+  const [count, setCount] = useState(1);
 
   return (
     <div className="AddItem">
@@ -11,11 +13,11 @@ const AddItem = () => {
       <img src={"/img/" + data.img} alt="" />
       <p className="price">{"$" + data.price}</p>
       <div className="counter-container">
-        <button>
+        <button onClick={() => setCount(count === 1 ? count : count - 1)}>
           <img src="/img/minus.png" alt="" />
         </button>
-        <p>1</p>
-        <button>
+        <p>{count}</p>
+        <button onClick={() => setCount(count >= 50 ? count : count + 1)}>
           <img src="/img/plus.png" alt="" />
         </button>
       </div>
