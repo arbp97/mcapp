9452263144f6dc8bccac8f65d4dda84c6a10af0c
Santiago Martinez <smartinezvila29@gmail.com>
2022-06-11 19:05:40 -0300

    Seccion Ofertas (con carrousel)
    Nueva seccion ofertas con carrousels

--- src/components/views/Discount.js
+++ src/components/views/Discount.js
@@ -1,11 +1,24 @@
-import offers from "../../data/offers.js";
-import OfferList from "../offers/OffersList.js";
+import ofertasSlider from "../../data/ofertasSlider.js"
+import Slider from "../slider/Slider.js"
+import "./Discount.css";
 
 const Discount = () => {
   return (
     <div className="discount">
       <p>Discount view</p>
-      <OfferList offers={offers}/>
+      <br></br>
+      <h3 className="title">{ofertasSlider.categoryCoffe}</h3>
+      <Slider items={ofertasSlider.itemsCoffe}/>
+      <br></br>
+      <h3 className="title">{ofertasSlider.category}</h3>
+      <Slider items={ofertasSlider.itemsBurgers}/>
+      <br></br>
+      <h3 className="title">{ofertasSlider.categoryHelado}</h3>
+      <Slider items={ofertasSlider.itemsHelado}/>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 };
---
+++ src/data/ofertasSlider.js
@@ -0,0 +1,64 @@
+// categories & products
+const ofertasSlider = {
+    category: "Para disfrutar en familia",
+    itemsBurgers: [
+      {
+        title: "MC Combo Cuadruple Bacon Grande",
+        img: "PROMOCIONE2McCombosMedianosCuartodeLibraconQueso.png",
+        price: "1090.00",
+      },
+      {
+        title: "MC Combo Grand Triple Tasty",
+        img: "Promocion20.png",
+        price: "990.00",
+      },
+      {
+        title: "MC Combo Grand Triple Tasty",
+        img: "Promocion18.png",
+        price: "990.00",
+      },
+      {
+        title: "MC Combo Grand Triple Tasty",
+        img: "Promocion19.png",
+        price: "990.00",
+      }
+    ],
+    categoryHelado: "Para que termines de la mejor manera",
+    itemsHelado: [
+      {
+        title: "2 Mc Flurry",
+        img: "Promocion15.png",
+        price: "599.00",
+      },
+      {
+        title: "2 sundae",
+        img: "Promocion16.png",
+        price: "300.00",
+      },
+      {
+        title: "2 conitos",
+        img: "Promocion12.png",
+        price: "150.00",
+      }
+    ],
+    categoryCoffe: "Arranca la mañana con fuerza",
+    itemsCoffe: [
+      {
+        title: "Cappuccino chico + 1 croissant",
+        img: "Promocion5.png",
+        price: "300.00",
+      },
+      {
+        title: "2 Cafes + 4 medialunas",
+        img: "Promocion7.png",
+        price: "390.00",
+      },
+      {
+        title: "Cappuccino chico + 2 medialunas",
+        img: "Promocion6.png",
+        price: "300.00",
+      }
+    ],
+  };
+  export default ofertasSlider;
+  
